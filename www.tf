6af1268d1158dcca93a65cceaac819b21abbe10f
Eric Härtel <eric.haertel@posteo.de>
2023-08-09 17:32:39 +0200

    TF rollout from github actions

--- www.tf
+++ www.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "ynra"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "ynra-infrastructure"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "www-bucket" {
   bucket = "ynra-www"
 
