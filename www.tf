ef1853e9a13e1c2e865939d7111b77e47271a73e
Eric Härtel <eric.haertel@posteo.de>
2023-08-10 07:12:37 +0200

    disable public access block

--- www.tf
+++ www.tf
@@ -26,7 +26,28 @@ resource "aws_s3_bucket_website_configuration" "www-bucket" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "www-bucket" {
+  bucket = aws_s3_bucket.www-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "www-bucket" {
+  bucket = aws_s3_bucket.www-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "www-bucket-acl" {
   bucket = aws_s3_bucket.www-bucket.id
-  acl    = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.www-bucket,
+    aws_s3_bucket_public_access_block.www-bucket
+  ]
+  acl = "public-read"
 }
